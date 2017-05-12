00173224777889ed7db95a56d6559922e01d70a1
冯尚实 <fengshangshi@fengshangshideMacBook-Pro.local>
2017-05-12 16:52:07 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @file Cache类
+ * @author ss.feng
+ */
+'use strict'
+
+const Cache = require('./libs/Cache');
+
+// 暴露cache接口
+exports.cache = new Cache;
---
+++ libs/Cache.js
@@ -0,0 +1,31 @@
+/**
+ * @file Cache类
+ * @author ss.feng
+ */
+'use strict'
+
+class Cache {
+    constructor() {
+        this.data = new Map;
+    }
+
+    // 设置
+    set(key, value) {
+        return this.data.set(key, value);
+    }
+
+    // 读取
+    get(key) {
+        return this.data.get(key);
+    }
+
+    // 删除
+    delete(key) {
+        return this.data.delete(key);
+    }
+
+    // 清空
+    clear() {
+        return this.data.clear();
+    }
+}
